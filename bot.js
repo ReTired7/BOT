6cc4adb2a3ea0de71e00ba382d43a228de3ced0e
ReTired7 <47307366+ReTired7@users.noreply.github.com>
2019-02-11 01:50:27 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ console.log("Scrpit By Dream");
 client.on("ready", () => {
 let channel =     client.channels.get("")
 setInterval(function() {
-channel.send(``);
+channel.send(`spam c__spam c___by_ReTir'ed___`);
 }, 30)
 })
 
